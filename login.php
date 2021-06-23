a7a13be09d37aab040bf9c4983ffa4533de25c22
leilaint <54875363+leilaint@users.noreply.github.com>
2021-06-23 16:03:25 +0800

    Update login.php

--- login.php
+++ login.php
@@ -34,7 +34,7 @@ function test_input($data){
 	<title>127</title>
 	<link rel="stylesheet" href="https://maxst.icons8.com/vue
 	-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
-	<link rel="stylesheet" href="login.css">
+	<link rel="stylesheet" href="style.css">
 </head>
 <body>
 	<input type="checkbox" id="nav-toggle">
